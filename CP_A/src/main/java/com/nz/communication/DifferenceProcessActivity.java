10ac0f49e5ed0789b5cce87e9ecc6808b40f4996
dell <zhouweiyong55@163.com>
2019-03-04 10:09:39 +0800

    2019/03/04

--- CP_A/src/main/java/com/nz/communication/DifferenceProcessActivity.java
+++ CP_A/src/main/java/com/nz/communication/DifferenceProcessActivity.java
@@ -3,7 +3,9 @@ package com.nz.communication;
 import android.app.Activity;
 import android.os.Bundle;
 
-
+/**
+ * 不同进程见cp_b
+ */
 public class DifferenceProcessActivity extends Activity {
 
     @Override
